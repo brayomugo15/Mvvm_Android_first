fcaf922705ad2f740cf6b0b87024c04b720be572
Brian Mugo <brayomugo2015@gmail.com>
2020-10-15 10:38:45 +0300

    Created : LoginViewModel class

--- app/src/main/java/com/project/mvvmandroidfirst/viewModels/LoginViewModel.java
+++ app/src/main/java/com/project/mvvmandroidfirst/viewModels/LoginViewModel.java
@@ -1,4 +1,66 @@
 package com.project.mvvmandroidfirst.viewModels;
 
-public class LoginViewModel {
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.project.mvvmandroidfirst.BR;
+import com.project.mvvmandroidfirst.models.User;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+    private String successMessage = "Login Successful";
+    private String errorMessage = "Email or password is Incorrect";
+
+    @Bindable
+    private String toastMessage = null;
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    public void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public void setUserEmail(String email){
+        user.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+
+    @Bindable
+    public String getUserEmail(){
+        return user.getEmail();
+    }
+
+    @Bindable
+    public String getUserPassword(){
+        return user.getPassword();
+    }
+
+    public void setUserPassword(String password){
+        user.setEmail(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public LoginViewModel(){
+        user = new User("", "");
+    }
+
+    public void onLoginClicked(){
+        if(isInputDataValid()){
+            setToastMessage(successMessage);
+        }else{
+            setToastMessage(errorMessage);
+        }
+    }
+
+    private boolean isInputDataValid() {
+        return !TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches()
+                && getUserPassword().length() > 5;
+    }
 }
